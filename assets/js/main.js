5f36ed21124b631b9d70aabdf0d3d08b804a5d30
chanvandaneb <ahyev42@gmai.com>
2022-03-21 00:29:17 +0700

    agency template

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,5 +6,21 @@
             delay: 100,
             time: 100000
         });
+
+        var mixer = mixitup('.items-wrapper');
+
+        let filterBtn = document.querySelectorAll('.filter-btn');
+
+
+        filterBtn.forEach((singleItem) => {
+            singleItem.addEventListener('click', function() {
+                filterBtn.forEach((val) => {
+                    val.classList.remove('active')
+                })
+                this.classList.add('active')
+            })
+        });
+
+        $('#select').niceSelect();
     })
  })(jQuery)
