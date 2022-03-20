643591f8465be2b808f3c3702723a0c1ece84a02
chanvandaneb <ahyev42@gmai.com>
2022-03-20 19:49:43 +0700

    practice project

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+;
+(function($) {
+    'use strict'
+    jQuery(document).ready(function() {
+        $('.counter').counterUp({
+            delay: 100,
+            time: 100000
+        });
+    })
+ })(jQuery)
