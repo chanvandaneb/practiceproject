c31e7f14920c6402aa9514988f1f6e89e763b35b
chanvandaneb <ahyev42@gmai.com>
2022-03-27 16:52:36 +0700

    agency-template

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,5 +22,70 @@
         });
 
         $('#select').niceSelect();
+
+        $(".testimonial-wrapper").slick({
+
+            // normal options...
+            infinite: false,
+            arrows: false,
+            dots: true,
+            slidesToShow: 2,
+            autoplay: true,
+          
+            // the magic
+            responsive: [{
+          
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  infinite: true
+                }
+          
+              }, {
+          
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  dots: true
+                }
+          
+              }, {
+          
+                breakpoint: 300,
+                settings: "unslick" // destroys slick
+          
+              }]
+          });
+
+        //   ACCORDION
+        $(".faq-content").css("display","none");
+        $(".faq-title").click(function () {
+            2
+              $(".faq-title").not(this).removeClass("open");
+            3
+              $(".faq-title").not(this).next().slideUp(300);
+            4
+              $(this).toggleClass("open");
+            5
+              $(this).next().slideToggle(300);
+            6
+        });
+            
+        const faqTitle = document.querySelectorAll('.faq-title');
+
+        faqTitle.forEach((value) => {
+            if (value.classList.contains('open')) {
+                value.nextElementSibling.style.display = 'block'
+                value.previousElementSibling.style.boxShadow = '0px 10px 60px 0px rgba(38 103 255 / 10%'
+            }
+            value.addEventListener('click', (e) => {
+                if (value.classList.contains('open')) {
+                    value.nextElementSibling.style.display = 'block'
+                    value.previousElementSibling.style.boxShadow = '0px 10px 60px 0px rgba(38 103 255 / 10%'
+                }
+            })
+        })
+
+
     })
  })(jQuery)
